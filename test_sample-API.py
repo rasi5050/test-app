67bc2afd5896a71c59995764e5a3c9c62f9a492d
Rasi <rasi@ll-kv-rasi.router6e4c04.com>
2019-05-16 12:06:37 +0530

    hello

--- test_sample-API.py
+++ test_sample-API.py
@@ -6,16 +6,11 @@ def test_api_req(supply_url):
 	url = supply_url + "/wishlist"
 	resp = requests.get(url)
 	r = resp.status_code	
-	
-	print(r)
-        assert resp.status_code == 200
-	assert resp.status_code == 200
-	assert resp.status_code == 200
 	assert resp.status_code == 200
 	
 
-if __name__ == '__main__':
-	test_api_req("https://www.usplworld.com")
+#if __name__ == '__main__':
+#	test_api_req("https://www.usplworld.com")
 	
 
 

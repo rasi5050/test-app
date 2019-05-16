8c7719103b5104b86584681172f2e44977ef6f45
Rasi <rasi@ll-kv-rasi.router6e4c04.com>
2019-05-16 11:41:13 +0530

    hello

---
+++ test_sample-API.py
@@ -0,0 +1,21 @@
+import pytest
+import requests
+import json
+
+def test_api_req(supply_url):
+	url = supply_url + "/wishlist"
+	resp = requests.get(url)
+	r = resp.status_code	
+	
+	print(r)
+        assert resp.status_code == 200
+	assert resp.status_code == 200
+	assert resp.status_code == 200
+	assert resp.status_code == 200
+	
+
+if __name__ == '__main__':
+	test_api_req("https://www.usplworld.com")
+	
+
+
